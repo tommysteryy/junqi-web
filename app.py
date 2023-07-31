b00d84b97c112040d47b610b604d89ba189947bf
Tommy Xu <tommysteryy@gmail.com>
2023-07-30 17:45:06 -0700

    changed to chinese version

--- app.py
+++ app.py
@@ -15,24 +15,28 @@ def result():
     blackCard = data['black']
 
     if junqi.isInvalid(redCard):
-        result = "The RED card name is invalid. Please try again."
+        result = "红牌输入错误，请重新输入"
     elif junqi.isInvalid(blackCard):
-        result = "The BLACK card name is invalid. Please try again."
+        result = "黑牌输入错误，请重新输入"
     else:
         winner, isGameOver = junqi.fight(redCard=redCard, blackCard=blackCard)
 
-        print(winner.name)
-        print(isGameOver.value)
-
         if (isGameOver.value):
-            result=f"{winner.name} has won the game!"
+            if (winner.name == "RED"):
+                result="红方抗走了黑方的军旗，黑方全军覆没！红方获胜！"
+            else:
+                result="黑方抗走了红方的军旗，红方全军覆没！黑方获胜！"
         elif (winner.name == "SELF_DESTRUCT"):
-            result="Red and Black self destruct."
+            result="红牌与黑牌同归于尽!"
         elif (winner.name == "IMPOSSIBLE"):
-            result="This combination of cards is impossible. Please recheck both of your inputs."
+            result="这两张牌不可能相遇，请双方再次核实后重新输入"
+        elif (winner.name == "RED"):
+            result="红牌战胜了黑牌!"
+        elif (winner.name == "BLACK"):
+            result="黑牌战胜了红牌!"
         else:
-            result=f"{winner.name} has the larger piece."
-            
+            result="网站发生了某些异常，请告知 Tommy (github.com/tommysteryy)，谢谢"
+
     return jsonify(result=result)
 
 if __name__ == '__main__':
